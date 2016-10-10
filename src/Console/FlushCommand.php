3ccad37253d10d0078e4aa71b64eaa87bfecdcaa
Giang Thai Cuong <thaicuong.giang@gmail.com>
2016-10-10 11:58:38 +0700

    re-enable syncing when an exception occurs in & change flush command && refine composer.json

--- src/Console/FlushCommand.php
+++ src/Console/FlushCommand.php
@@ -4,21 +4,21 @@ namespace Gtk\Larasearch\Console;
 
 use Illuminate\Console\Command;
 
-class ClearCommand extends Command
+class FlushCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'larasearch:clear {model}';
+    protected $signature = 'larasearch:flush {model}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Clear the given model from the search index';
+    protected $description = "Flush all of the model's records from the index";
 
     /**
      * Execute the console command.
@@ -33,6 +33,6 @@ class ClearCommand extends Command
 
         $model::removeAllFromSearch();
 
-        $this->info('All ['.$class.'] records have been cleared.');
+        $this->info('All ['.$class.'] records have been flushed.');
     }
 }
