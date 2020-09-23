582f29c6657018003e10f6672ed6e9b8d40a0505
Jason McCreary <jason@pureconcepts.net>
2020-09-23 10:12:08 -0400

    Get PHPUnit tests running

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -5,7 +5,7 @@ namespace Tests;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
-abstract class AbstractTestCase extends PHPUnit\Framework\TestCase
+abstract class AbstractTestCase extends TestCase
 {
     protected function tearDown(): void
     {
