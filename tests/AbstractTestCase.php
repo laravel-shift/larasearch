1925c663a9ad4a2d6363350a2eac45bb6a024bed
Laravel Shift <shift@laravelshift.com>
2020-09-23 10:06:40 -0400

    PHPUnit 8 Shift (#3)

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends PHPUnit\Framework\TestCase
 {
-    public function tearDown()
+    protected function tearDown(): void
     {
         Mockery::close();
 
